d1d1fc2457f96563ae3b9a457f821c238470f9d5
Pranit <patilpranit3112@gmail.com>
2023-11-07 18:16:37 +0530

    The Hero Section Completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,32 @@
+import { logo } from "../assets";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex flex-col justify-center items-center">
+      <nav className="flex justify-between items-center w-full mb-10 pt-5">
+        <img src={logo} className="w-28 object-contain" />
+        <button
+          type="button"
+          className="black_btn"
+          onClick={() =>
+            window.open("https://github.com/PranitPatil03/ai_summarizer")
+          }
+        >
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT 4</span>
+      </h1>
+
+      <h2 className="desc_text">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
